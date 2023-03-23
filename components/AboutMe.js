534203b3efab78b79c9d3215439029375dd75434
cdizon239 <coleencharmille@gmail.com>
2023-03-22 23:13:01 -0400

    style tweaks

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,8 +4,8 @@ import userData from "@constants/data";
 export default function AboutMe() {
   return (
     <section className="bg-white dark:bg-gray-800">
-      <div className="max-w-6xl mx-auto h-42 bg-white dark:bg-gray-200">
-        <h1 className="text-5xl md:text-5xl font-bold py-20 text-center md:text-left md:mx-3 text-gray-600">
+      <div className="max-w-6xl mx-auto h-42 bg-[#F1F1F1] dark:bg-gray-200">
+        <h1 className="text-5xl md:text-5xl font-bold py-10 text-center md:text-left md:mx-3 text-gray-600">
           About Me
         </h1>
       </div>
@@ -19,26 +19,24 @@ export default function AboutMe() {
           </p>
         </div>
       </div>
-      <div className="bg-[#F1F1F1] dark:bg-gray-200 px-4 py-4">
-        <div className="pt-5 max-w-6xl mx-auto">
+      <div className="bg-[#F1F1F1] dark:bg-gray-900">
+        <div className="pt-5 max-w-6xl mx-auto  dark:bg-gray-200">
           {/* Text area */}
-          <div className="col-span-1 mx-auto">
-            <div className="max-w-6xl py-5">
-              <h1 className="text-5xl font-bold text-center md:text-left text-gray-600">
-                Skills
-              </h1>
-            </div>
-            <div className="bg-[#F1F1F1] dark:bg-gray-200 dark:text-gray-800">
-              <div className="text-container mx-auto">
-                {userData.about.description?.map((desc, idx) => (
-                  <p
-                    key={idx}
-                    className="leading-loose text-l md:text-xl  mx-4 py-2"
-                  >
-                    {desc}
-                  </p>
-                ))}
-              </div>
+          <div className="max-w-6xl py-5">
+            <h1 className="text-5xl font-bold text-center md:text-left md:mx-3 text-gray-600">
+              Skills
+            </h1>
+          </div>
+          <div className="bg-[#F1F1F1] dark:bg-gray-200 dark:text-gray-800 pb-10">
+            <div className="text-container mx-auto">
+              {userData.about.description?.map((desc, idx) => (
+                <p
+                  key={idx}
+                  className="leading-loose text-l md:text-xl  mx-4 py-2"
+                >
+                  {desc}
+                </p>
+              ))}
             </div>
           </div>
         </div>
